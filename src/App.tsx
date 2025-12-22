d2f8632ff570aaa5ca9ec21e237445d101656508
LineUp-LC <lineupdownbiz@gmail.com>
2025-12-22 00:31:24 -0600

    Remove PasswordGate and fix App.tsx for public waitlist

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,14 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./router";
 import { I18nextProvider } from "react-i18next";
 import i18n from "./i18n";
-import PasswordGate from "./PasswordGate";
 
 function App() {
   return (
-    <PasswordGate>
-      <I18nextProvider i18n={i18n}>
-        <BrowserRouter basename={__BASE_PATH__}>
-          <AppRoutes />
-        </BrowserRouter>
-      </I18nextProvider>
-    </PasswordGate>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter basename={__BASE_PATH__}>
+        <AppRoutes />
+      </BrowserRouter>
+    </I18nextProvider>
   );
 }
 
